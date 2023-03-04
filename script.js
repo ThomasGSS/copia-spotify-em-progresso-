217aa952a359c231bc8cba61af686827f28c8a7b
thomas gabriel santos da silva <125462321+ThomasGSS@users.noreply.github.com>
2023-03-04 19:57:26 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const songName = document.getElementById("song-name")
+const song = document.getElementById('audio');
+const play = document.getElementById('play');
+
+songName.innerText = 'Atravessei São Paulo';
+let isPlaying = false;
+
+function playSong(){
+    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
+    song.play();
+    isPlaying = true;
+}   
+function pauseSong(){
+    play.querySelector('.bi').classList.add('bi-play-circle-fill');
+    play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
+    song.pause();
+    isPlaying = false;
+}
+
+function playPauseDecider(){
+    if(isPlaying === true){
+        pauseSong();
+    }
+    else {
+        playSong();
+    }
+}
+
+play.addEventListener('click', playPauseDecider)
